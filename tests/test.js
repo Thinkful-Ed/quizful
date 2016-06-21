bcf550ea30c65a51268ad49a9c9d4c03bfd999b5
Aric Johnson <aric87@comcast.net>
2016-06-21 20:07:40 +0000

    add test for :language route

--- tests/test.js
+++ tests/test.js
@@ -19,6 +19,11 @@ describe('Quizful', () => {
             'correctAnswer':'test answer',
             'possibleAnswers':['test answer 1','test answer 2','test answer 3'],
             'language':'HTML'
+        },{
+            'text':'test text',
+            'correctAnswer':'test answer',
+            'possibleAnswers':['test answer 1','test answer 2','test answer 3'],
+            'language':'javascript'
         },
          (err, items) => {
             if (err) {
@@ -42,7 +47,7 @@ describe('Quizful', () => {
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('array');
-                res.body.should.be.length(1);
+                res.body.should.be.length(2);
                 res.body[0].should.be.a('object');
                 res.body[0].should.have.property('_id');
                 res.body[0].should.have.property('text');
@@ -55,6 +60,29 @@ describe('Quizful', () => {
                 res.body[0].possibleAnswers[0].should.equal('test answer 1')
                 res.body[0].should.have.property('language');
                 res.body[0].language.should.equal('HTML')
+                done();
+            });
+    });
+    it('should list language specific questions on get/:language', (done) => {
+        chai.request(app)
+            .get('/questions/javascript')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.should.be.length(1);
+                res.body[0].should.be.a('object');
+                res.body[0].should.have.property('_id');
+                res.body[0].should.have.property('text');
+                res.body[0].text.should.equal('test text');
+                res.body[0].should.have.property('correctAnswer');
+                res.body[0].correctAnswer.should.equal('test answer')
+                res.body[0].should.have.property('possibleAnswers');
+                res.body[0].possibleAnswers.should.be.a('array');
+                res.body[0].possibleAnswers.should.be.length(3);
+                res.body[0].possibleAnswers[0].should.equal('test answer 1')
+                res.body[0].should.have.property('language');
+                res.body[0].language.should.equal('javascript')
                 done();
             });
     });
